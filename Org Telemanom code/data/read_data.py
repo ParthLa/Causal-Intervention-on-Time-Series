1f57ef9bdb2eabb5d4328c2b9aa0801e6468f1de
ParthLa <parthlaturia@gmail.com>
2021-09-23 00:17:51 +0530

    Used avg to label anomalies in a sequence

--- Org Telemanom code/data/read_data.py
+++ Org Telemanom code/data/read_data.py
@@ -7,20 +7,20 @@ test = 'test/'
 train_lbl = 'train_lbl.txt'
 test_lbl = 'test_lbl.txt'
 
-# A1_train = np.load(train+'A-1.npy')
-# A1_test = np.load(test+'A-1.npy')
-# # print(A1_train.shape)
-# # print(A1_test.shape)
-# print(A1_test[4700:4710])
+# A1_train = np.load(train+'P-2.npy')
+# A1_test = np.load(test+'P-2.npy')
+# print(A1_train.shape)
+# print(A1_test.shape)
+# print(A1_test[0:10]) # normal
 # print("------------------")
-# print(A1_test[4600:4610])
+# print(A1_test[6000:6010]) # anomaly
+# print("------------------")
+# # print(A1_test[4600:4610]) # normal
 
-def anomaly(vec):
+def anomaly(vec, avg, delta):
 	temp=0
-	if(np.sum(vec)<=1):
+	if (abs(vec[0]-avg)>delta):
 		temp=1
-	# elif(vec[0]!=1):
-	# 	temp=1
 	return temp
 
 with open(train_lbl, 'w') as t:
@@ -33,7 +33,15 @@ with open(train_lbl, 'w') as t:
 			ans = np.zeros(l)
 			for i in range(l):
 				vec = f_train[i]
-				if(anomaly(vec)==1):
+				avg = 0
+				length = 3
+				lb = max(0, i-length)
+				for j in range(lb, i):
+					avg+=f_train[j][0]
+				if(i!=0):
+					avg = avg/(i - lb) 
+				delta = 0.5
+				if(anomaly(vec, avg, delta)==1):
 					ans[i]=1
 			t.write(file)
 			t.write('\n')
@@ -51,7 +59,15 @@ with open(test_lbl, 'w') as t:
 			ans = np.zeros(l)
 			for i in range(l):
 				vec = f_test[i]
-				if(anomaly(vec)==1):
+				avg = 0
+				length = 3
+				lb = max(0, i-length)
+				for j in range(lb, i):
+					avg+=f_test[j][0]
+				if(i!=0):
+					avg = avg/(i - lb) 
+				delta = 0.5
+				if(anomaly(vec, avg, delta)==1):
 					ans[i]=1
 			t.write(file)
 			t.write('\n')
